16bbbe19c5dfe299e6058a44c9dfd3bedd93a17b
MADHAN <madhansmart7404@gmail.com>
2018-11-26 18:32:30 +0530

    Updates

---
+++ precache-manifest.89d041f9e819462365f6586dfa2e25d3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bae60c2b3e2ec343b18a3d05df6a653",
+    "url": "/FoodDelivery/static/media/page-img-3.8bae60c2.jpg"
+  },
+  {
+    "revision": "8e4daec9044489b3b4d9",
+    "url": "/FoodDelivery/static/css/main.6b6f4d35.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/FoodDelivery/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9dc9725584737ca7c7f6",
+    "url": "/FoodDelivery/static/js/1.9dc97255.chunk.js"
+  },
+  {
+    "revision": "57149ca97e8698490d10",
+    "url": "/FoodDelivery/static/js/runtime~main.57149ca9.js"
+  },
+  {
+    "revision": "ab9e4850913a44042dc193d988a51950",
+    "url": "/FoodDelivery/static/media/main.ab9e4850.jpg"
+  },
+  {
+    "revision": "ebe514f792a2952373119eae316a92bd",
+    "url": "/FoodDelivery/static/media/page-img.ebe514f7.jpg"
+  },
+  {
+    "revision": "20aef33985c40492e784d09ee0bf7160",
+    "url": "/FoodDelivery/static/media/page-img-1.20aef339.jpg"
+  },
+  {
+    "revision": "15deaafa355169c34294a3df6d158fea",
+    "url": "/FoodDelivery/static/media/page-img-2.15deaafa.jpg"
+  },
+  {
+    "revision": "8e4daec9044489b3b4d9",
+    "url": "/FoodDelivery/static/js/main.8e4daec9.chunk.js"
+  },
+  {
+    "revision": "298efd885039bd7e17e509d8b3e3f13e",
+    "url": "/FoodDelivery/static/media/bg-block.298efd88.png"
+  },
+  {
+    "revision": "448db51a97693b9efb6bd9d0ec539bb9",
+    "url": "/FoodDelivery/static/media/bg-block-2.448db51a.png"
+  },
+  {
+    "revision": "032499e928ba0a42795d50d71bc5bea8",
+    "url": "/FoodDelivery/static/media/bg-block-1.032499e9.png"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/FoodDelivery/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/FoodDelivery/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/FoodDelivery/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/FoodDelivery/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "9dc9725584737ca7c7f6",
+    "url": "/FoodDelivery/static/css/1.2404481d.chunk.css"
+  },
+  {
+    "revision": "1ecc5dc87a58861743d2a30982da929e",
+    "url": "/FoodDelivery/index.html"
+  }
+];
